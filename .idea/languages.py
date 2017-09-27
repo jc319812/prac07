d34627cc3d1d953072d23b87a1988aae8533e6a6
jc319812 <abdalnasser.zamara@my.jcu.edu.au>
2017-09-28 00:28:03 +1000

    prac07

--- .idea/languages.py
+++
@@ -1,9 +0,0 @@
-from prac07.programminglanguage import ProgrammingLanguage
-
-languages = [ProgrammingLanguage("Ruby", "Dynamic", True, 1995), ProgrammingLanguage("Python", "Dynamic", True, 1991),
-			 ProgrammingLanguage("Visual Basic", "Static", False, 1991)]
-
-print("The dynamically typed languages are")
-for language in languages:
-	if language.is_dynamic():
-		print(language.name)
